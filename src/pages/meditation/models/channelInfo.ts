0a78b3b51f194d25e97c8e3e1587c1ebc313e11f
Park Nayeon <within197@naver.com>
2024-11-28 23:48:48 +0900

    ✨ Feat: meditation 채널 상수 추가

--- src/pages/meditation/models/channelInfo.ts
+++ src/pages/meditation/models/channelInfo.ts
@@ -7,9 +7,30 @@ import {
 } from '@pages/meditation/constants';
 
 export const meditationChannelInfo = new Map([
-  [CONCENTRATION_KEY, { label: '집중', id: '65017a41dfe8db5726b603a7' }],
-  [ANXIETY_KEY, { label: '불안', id: '65003530a72a0d2e63f12878' }],
+  [CONCENTRATION_KEY, { label: '집중', id: '1' }],
+  [ANXIETY_KEY, { label: '불안', id: '2' }],
   [FREEDOM_KEY, { label: '자유', id: '65017abcdfe8db5726b603c3' }],
   [RELAXATION_KEY, { label: '휴식', id: '65017aa2dfe8db5726b603ba' }],
   [STRESS_KEY, { label: '스트레스', id: '65017a5edfe8db5726b603b1' }]
 ]);
+
+type ChannelValue = {
+  label: string;
+  id: string;
+};
+
+export interface meditationChannelsInfo {
+  [CONCENTRATION_KEY]: ChannelValue;
+  [ANXIETY_KEY]: ChannelValue;
+  [FREEDOM_KEY]: ChannelValue;
+  [RELAXATION_KEY]: ChannelValue;
+  [STRESS_KEY]: ChannelValue;
+}
+
+export const meditationChannelsInfo: meditationChannelsInfo = {
+  [CONCENTRATION_KEY]: { label: '집중', id: '1' },
+  [ANXIETY_KEY]: { label: '불안', id: '2' },
+  [FREEDOM_KEY]: { label: '자유', id: '65017abcdfe8db5726b603c3' },
+  [RELAXATION_KEY]: { label: '휴식', id: '65017aa2dfe8db5726b603ba' },
+  [STRESS_KEY]: { label: '스트레스', id: '65017a5edfe8db5726b603b1' }
+};
