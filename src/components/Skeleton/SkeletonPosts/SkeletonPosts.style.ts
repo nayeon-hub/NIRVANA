0fdb77060994d6ae71b54b8dab4492589d998d0b
Park Nayeon <within197@naver.com>
2024-12-30 22:28:31 +0900

    💄 Style: Posts height 수정

--- src/components/Skeleton/SkeletonPosts/SkeletonPosts.style.ts
+++ src/components/Skeleton/SkeletonPosts/SkeletonPosts.style.ts
@@ -2,6 +2,7 @@ import styled from '@emotion/styled';
 
 export const SkeletonPostsPage = styled.div`
   width: 100%;
-  height: 100%;
+  height: calc(100vh - 64px - 50px - 70px);
   background-color: ${({ theme }) => theme.color.white};
+  overflow: hidden;
 `;
--- src/pages/posts/Posts.style.ts
+++ src/pages/posts/Posts.style.ts
@@ -2,7 +2,6 @@ import styled from '@emotion/styled';
 
 const StyledPostsPage = styled.div`
   width: 100%;
-  height: 100vh;
   background-color: ${({ theme }) => theme.color.white};
 `;
 
